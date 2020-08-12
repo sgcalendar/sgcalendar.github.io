6948ab2caca4fbdc694b8781c195575e610e67d4
sgcalendar <59480807+sgcalendar@users.noreply.github.com>
2020-08-12 16:21:40 +0100

    Update data.js

--- data.js
+++ data.js
@@ -660,7 +660,7 @@ var jsonData = {
                 "e": [{
                     "i": "https://i.imgur.com/O6Irvei.png",
                     "u": "https://i.imgur.com/nFRv4gz.png",
-                    "t": "16:00",
+                    "t": "16:30",
                     "n": "Metin Spawn"
                 },
                 {
