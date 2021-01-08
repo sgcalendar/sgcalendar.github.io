2cf3a1c43305962093d9a4a688d17d9957e91101
sgcalendar <59480807+sgcalendar@users.noreply.github.com>
2021-01-08 15:06:31 +0000

    Update data.js

--- data.js
+++ data.js
@@ -19,7 +19,7 @@ var jsonData = {
                 "g": null,
                 "t": null,
                 "n": null
-            },{
+            }, {
                 "d": 0,
                 "i": "./images/logo.png",
                 "g": null,
@@ -49,48 +49,48 @@ var jsonData = {
             {
                 "d": 2,
                 "e": [{
-                        "i": "https://i.imgur.com/5HTFviS.png",
-                        "t": "All Day",
-                        "n": "Mystery Box",
-                        "u": "https://i.imgur.com/kB9HwLy.png"
-                    },
-                    {
-                        "i": "https://i.imgur.com/LmwH11a.png",
-                        
-                        "t": "17:00",
-                        "n": "Budokan PvP"
-                    },
-                    {
-                        "i": "https://i.imgur.com/SRFNp3n.png",
-                        "t": "15:00",
-                        "n": "Fishing Extravaganza",
-                        "f": 0
-                    }
+                    "i": "https://i.imgur.com/5HTFviS.png",
+                    "t": "All Day",
+                    "n": "Mystery Box",
+                    "u": "https://i.imgur.com/kB9HwLy.png"
+                },
+                {
+                    "i": "https://i.imgur.com/LmwH11a.png",
+
+                    "t": "17:00",
+                    "n": "Budokan PvP"
+                },
+                {
+                    "i": "https://i.imgur.com/SRFNp3n.png",
+                    "t": "15:00",
+                    "n": "Fishing Extravaganza",
+                    "f": 0
+                }
                 ]
             },
             {
                 "d": 3,
                 "e": [{
-                        "i": "https://i.imgur.com/5HTFviS.png",
-                        "t": "All Day",
-                        "n": "Mystery Box",
-                        "u": "https://i.imgur.com/kB9HwLy.png"
-                    },
-                    {
-                        "i": "./images/logo.png",
-                        "g": "",
-                        "t": "17:00",
-                        "n": "Nation War"
-                    }
+                    "i": "https://i.imgur.com/5HTFviS.png",
+                    "t": "All Day",
+                    "n": "Mystery Box",
+                    "u": "https://i.imgur.com/kB9HwLy.png"
+                },
+                {
+                    "i": "./images/logo.png",
+                    "g": "",
+                    "t": "17:00",
+                    "n": "Nation War"
+                }
                 ]
             },
             {
                 "d": 4,
-                        "i": "https://i.imgur.com/dAm7jFf.png",
-                        "t": "16:00",
-                        "n": "Vein Spawn",
-                        "u": "https://i.imgur.com/JOiieaI.jpg",
-                        "f": 0
+                "i": "https://i.imgur.com/dAm7jFf.png",
+                "t": "16:00",
+                "n": "Vein Spawn",
+                "u": "https://i.imgur.com/JOiieaI.jpg",
+                "f": 0
             },
             {
                 "d": 5,
@@ -111,78 +111,77 @@ var jsonData = {
             {
                 "d": 7,
                 "e": [{
-                        "i": "https://i.imgur.com/IllUyVq.png",
-                        "t": "20:00",
-                        "n": "OX Event",
-                        "u": "https://i.imgur.com/Kc7IsVi.jpg",
-                        "f": 0
-                    },
-                    {
-                        "i": "https://i.imgur.com/zGm6lA5.png",
-                        "u": "https://i.imgur.com/9BgHnmP.jpg",
-                        "t": "All Day",
-                        "n": "Moonlight Box"
-                    } 
+                    "i": "https://i.imgur.com/IllUyVq.png",
+                    "t": "20:00",
+                    "n": "OX Event",
+                    "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                    "f": 0
+                },
+                {
+                    "i": "https://i.imgur.com/zGm6lA5.png",
+                    "u": "https://i.imgur.com/9BgHnmP.jpg",
+                    "t": "All Day",
+                    "n": "Moonlight Box"
+                }
                 ]
             },
             {
                 "d": 8,
-                "i": "https://i.imgur.com/M015dld.png",
+                "i": "./images/logo.png",
                 "t": "16:00",
-                "n": "DT Run",
-                "u": "https://i.imgur.com/l9rRJLi.jpg"
+                "n": "Hunter's Hour"
             },
             {
                 "d": 9,
                 "e": [{
-                        "i": "https://i.imgur.com/nnJgE4E.png",
-                        "t": "All Day",
-                        "n": "Hexagonal Box",
-                        "u": "https://i.imgur.com/kgPPMkZ.jpg"
-                    },
-                    {
-                        "i": "https://i.imgur.com/LmwH11a.png",
-                        
-                        "t": "17:00",
-                        "n": "Budokan PvP"
-                    },
-                    {
-                        "i": "https://i.imgur.com/SRFNp3n.png",
-                        "t": "15:00",
-                        "n": "Fishing Extravaganza",
-                        "f": 0
-                    }
+                    "i": "https://i.imgur.com/nnJgE4E.png",
+                    "t": "All Day",
+                    "n": "Hexagonal Box",
+                    "u": "https://i.imgur.com/kgPPMkZ.jpg"
+                },
+                {
+                    "i": "https://i.imgur.com/LmwH11a.png",
+
+                    "t": "17:00",
+                    "n": "Budokan PvP"
+                },
+                {
+                    "i": "https://i.imgur.com/SRFNp3n.png",
+                    "t": "15:00",
+                    "n": "Fishing Extravaganza",
+                    "f": 0
+                }
                 ]
             },
             {
                 "d": 10,
                 "e": [{
-                        "i": "https://i.imgur.com/nnJgE4E.png",
-                        "t": "All Day",
-                        "n": "Hexagonal Box",
-                        "u": "https://i.imgur.com/kgPPMkZ.jpg"
-                    },
-                    {
-                        "i": "./images/logo.png",
-                        "g": "",
-                        "t": "15:00",
-                        "n": "Nation War"
-                    }
+                    "i": "https://i.imgur.com/nnJgE4E.png",
+                    "t": "All Day",
+                    "n": "Hexagonal Box",
+                    "u": "https://i.imgur.com/kgPPMkZ.jpg"
+                },
+                {
+                    "i": "./images/logo.png",
+                    "g": "",
+                    "t": "15:00",
+                    "n": "Nation War"
+                }
                 ]
             },
             {
                 "d": 11,
                 "e": [{
-                        "i": "./images/logo.png",
-                        "t": "16:00",
-                        "n": "Tanaka Hunt"
-                    },
-                    {
-                        "i": "https://i.imgur.com/zGm6lA5.png",
-                        "u": "https://i.imgur.com/9BgHnmP.jpg",
-                        "t": "All Day",
-                        "n": "Moonlight Box"
-                    }
+                    "i": "./images/logo.png",
+                    "t": "16:00",
+                    "n": "Tanaka Hunt"
+                },
+                {
+                    "i": "https://i.imgur.com/zGm6lA5.png",
+                    "u": "https://i.imgur.com/9BgHnmP.jpg",
+                    "t": "All Day",
+                    "n": "Moonlight Box"
+                }
                 ]
             },
             {
@@ -217,73 +216,73 @@ var jsonData = {
             {
                 "d": 16,
                 "e": [{
-                        "i": "https://i.imgur.com/5HTFviS.png",
-                        "t": "All Day",
-                        "n": "Mystery Box",
-                        "u": "https://i.imgur.com/kB9HwLy.png"
-                    },
-                    {
-                        "i": "https://i.imgur.com/LmwH11a.png",
-                        
-                        "t": "17:00",
-                        "n": "Budokan PvP"
-                    },
-                    {
-                        "i": "https://i.imgur.com/SRFNp3n.png",
-                        "t": "15:00",
-                        "n": "Fishing Extravaganza",
-                        "f": 0
-                    }
+                    "i": "https://i.imgur.com/5HTFviS.png",
+                    "t": "All Day",
+                    "n": "Mystery Box",
+                    "u": "https://i.imgur.com/kB9HwLy.png"
+                },
+                {
+                    "i": "https://i.imgur.com/LmwH11a.png",
+
+                    "t": "17:00",
+                    "n": "Budokan PvP"
+                },
+                {
+                    "i": "https://i.imgur.com/SRFNp3n.png",
+                    "t": "15:00",
+                    "n": "Fishing Extravaganza",
+                    "f": 0
+                }
                 ]
             },
             {
                 "d": 17,
                 "e": [{
-                        "i": "https://i.imgur.com/5HTFviS.png",
-                        "t": "All Day",
-                        "n": "Mystery Box",
-                        "u": "https://i.imgur.com/kB9HwLy.png"
-                    },
-                    {
-                        "i": "./images/logo.png",
-                        "g": "",
-                        "t": "16:00",
-                        "n": "Nation War"
-                    }
+                    "i": "https://i.imgur.com/5HTFviS.png",
+                    "t": "All Day",
+                    "n": "Mystery Box",
+                    "u": "https://i.imgur.com/kB9HwLy.png"
+                },
+                {
+                    "i": "./images/logo.png",
+                    "g": "",
+                    "t": "16:00",
+                    "n": "Nation War"
+                }
                 ]
             },
             {
                 "d": 18,
-                        "i": "https://i.imgur.com/dAm7jFf.png",
-                        "t": "16:00",
-                        "n": "Vein Spawn",
-                        "u": "https://i.imgur.com/JOiieaI.jpg",
-                        "f": 0
+                "i": "https://i.imgur.com/dAm7jFf.png",
+                "t": "16:00",
+                "n": "Vein Spawn",
+                "u": "https://i.imgur.com/JOiieaI.jpg",
+                "f": 0
             },
             {
                 "d": 19,
                 "e": [{
-                        "i": "https://i.imgur.com/IllUyVq.png",
-                        "t": "16:00",
-                        "n": "OX Event",
-                        "u": "https://i.imgur.com/Kc7IsVi.jpg",
-                        "f": 0
-                    },
-                    {
-                        "i": "https://i.imgur.com/zGm6lA5.png",
-                        "u": "https://i.imgur.com/9BgHnmP.jpg",
-                        "t": "All Day",
-                        "n": "Moonlight Box"
-                    }
+                    "i": "https://i.imgur.com/IllUyVq.png",
+                    "t": "16:00",
+                    "n": "OX Event",
+                    "u": "https://i.imgur.com/Kc7IsVi.jpg",
+                    "f": 0
+                },
+                {
+                    "i": "https://i.imgur.com/zGm6lA5.png",
+                    "u": "https://i.imgur.com/9BgHnmP.jpg",
+                    "t": "All Day",
+                    "n": "Moonlight Box"
+                }
                 ]
             },
             {
                 "d": 20,
-                        "i": "https://i.imgur.com/CQMpPqr.png",
-                        "t": "16:00",
-                        "u": "https://i.imgur.com/UzL4RIf.jpg",
-                        "n": "Boss Spawn",
-                        "g": "https://www.metin2sg.com/forum/thread/7167-event-boss-spawn/"
+                "i": "https://i.imgur.com/CQMpPqr.png",
+                "t": "16:00",
+                "u": "https://i.imgur.com/UzL4RIf.jpg",
+                "n": "Boss Spawn",
+                "g": "https://www.metin2sg.com/forum/thread/7167-event-boss-spawn/"
             },
             {
                 "d": 21,
@@ -303,47 +302,47 @@ var jsonData = {
             {
                 "d": 23,
                 "e": [{
-                        "i": "https://i.imgur.com/nnJgE4E.png",
-                        "t": "All Day",
-                        "n": "Hexagonal Box",
-                        "u": "https://i.imgur.com/kgPPMkZ.jpg"
-                    },
-                    {
-                        "i": "https://i.imgur.com/LmwH11a.png",
-                        
-                        "t": "17:00",
-                        "n": "Budokan PvP"
-                    },
-                    {
-                        "i": "https://i.imgur.com/SRFNp3n.png",
-                        "t": "15:00",
-                        "n": "Fishing Extravaganza",
-                        "f": 0
-                    }
+                    "i": "https://i.imgur.com/nnJgE4E.png",
+                    "t": "All Day",
+                    "n": "Hexagonal Box",
+                    "u": "https://i.imgur.com/kgPPMkZ.jpg"
+                },
+                {
+                    "i": "https://i.imgur.com/LmwH11a.png",
+
+                    "t": "17:00",
+                    "n": "Budokan PvP"
+                },
+                {
+                    "i": "https://i.imgur.com/SRFNp3n.png",
+                    "t": "15:00",
+                    "n": "Fishing Extravaganza",
+                    "f": 0
+                }
                 ]
             },
             {
                 "d": 24,
                 "e": [{
-                        "i": "https://i.imgur.com/nnJgE4E.png",
-                        "t": "All Day",
-                        "n": "Hexagonal Box",
-                        "u": "https://i.imgur.com/kgPPMkZ.jpg"
-                    },
-                    {
-                        "i": "./images/logo.png",
-                        "g": "",
-                        "t": "17:00",
-                        "n": "Nation War"
-                    }
+                    "i": "https://i.imgur.com/nnJgE4E.png",
+                    "t": "All Day",
+                    "n": "Hexagonal Box",
+                    "u": "https://i.imgur.com/kgPPMkZ.jpg"
+                },
+                {
+                    "i": "./images/logo.png",
+                    "g": "",
+                    "t": "17:00",
+                    "n": "Nation War"
+                }
                 ]
             },
             {
                 "d": 25,
-                    "i": "./images/logo.png",
-                    "t": "16:00",
-                    "n": "Tanaka Hunt"
-                
+                "i": "./images/logo.png",
+                "t": "16:00",
+                "n": "Tanaka Hunt"
+
             },
             {
                 "d": 26,
@@ -356,17 +355,17 @@ var jsonData = {
             {
                 "d": 27,
                 "e": [{
-                        "i": "https://i.imgur.com/O6Irvei.png",
-                        "u": "https://i.imgur.com/nFRv4gz.png",
-                        "t": "16:00",
-                        "n": "Metin Spawn"
-                    },
-                    {
-                        "i": "https://i.imgur.com/zGm6lA5.png",
-                        "u": "https://i.imgur.com/9BgHnmP.jpg",
-                        "t": "All Day",
-                        "n": "Moonlight Box"
-                    }
+                    "i": "https://i.imgur.com/O6Irvei.png",
+                    "u": "https://i.imgur.com/nFRv4gz.png",
+                    "t": "16:00",
+                    "n": "Metin Spawn"
+                },
+                {
+                    "i": "https://i.imgur.com/zGm6lA5.png",
+                    "u": "https://i.imgur.com/9BgHnmP.jpg",
+                    "t": "All Day",
+                    "n": "Moonlight Box"
+                }
                 ]
             },
             {
@@ -386,47 +385,47 @@ var jsonData = {
             {
                 "d": 30,
                 "e": [{
-                        "i": "https://i.imgur.com/5HTFviS.png",
-                        "t": "All Day",
-                        "n": "Mystery Box",
-                        "u": "https://i.imgur.com/kB9HwLy.png"
-                    },
-                    {
-                        "i": "https://i.imgur.com/LmwH11a.png",
-                        
-                        "t": "17:00",
-                        "n": "Budokan PvP"
-                    },
-                    {
-                        "i": "https://i.imgur.com/SRFNp3n.png",
-                        "t": "15:00",
-                        "n": "Fishing Extravaganza",
-                        "f": 0
-                    }
+                    "i": "https://i.imgur.com/5HTFviS.png",
+                    "t": "All Day",
+                    "n": "Mystery Box",
+                    "u": "https://i.imgur.com/kB9HwLy.png"
+                },
+                {
+                    "i": "https://i.imgur.com/LmwH11a.png",
+
+                    "t": "17:00",
+                    "n": "Budokan PvP"
+                },
+                {
+                    "i": "https://i.imgur.com/SRFNp3n.png",
+                    "t": "15:00",
+                    "n": "Fishing Extravaganza",
+                    "f": 0
+                }
                 ]
             },
             {
                 "d": 31,
                 "e": [{
-                        "i": "https://i.imgur.com/5HTFviS.png",
-                        "t": "All Day",
-                        "n": "Mystery Box",
-                        "u": "https://i.imgur.com/kB9HwLy.png"
-                    },
-                    {
-                        "i": "./images/logo.png",
-                        "g": "",
-                        "t": "15:00",
-                        "n": "Nation War"
-                    },
-                    {
-                        "i": "./images/logo.png",
-                        "t": "14:00",
-                        "n": "Dungeon Masters"
-                    }
+                    "i": "https://i.imgur.com/5HTFviS.png",
+                    "t": "All Day",
+                    "n": "Mystery Box",
+                    "u": "https://i.imgur.com/kB9HwLy.png"
+                },
+                {
+                    "i": "./images/logo.png",
+                    "g": "",
+                    "t": "15:00",
+                    "n": "Nation War"
+                },
+                {
+                    "i": "./images/logo.png",
+                    "t": "14:00",
+                    "n": "Dungeon Masters"
+                }
                 ]
             }
-            
+
         ]
     }
 }
